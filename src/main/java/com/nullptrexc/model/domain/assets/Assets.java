9e12387219d3f81582bc2a6bd5d21895e6a835fe
null/ <null.ptr.exc@outlook.com>
2025-03-24 22:05:32 +0100

    + Assets added

--- src/main/java/com/nullptrexc/model/domain/assets/Assets.java
+++ src/main/java/com/nullptrexc/model/domain/assets/Assets.java
@@ -3,4 +3,16 @@ package com.nullptrexc.model.domain.assets;
 public class Assets {
     private String namespace;
 
+    public String getNamespace() {
+        return namespace;
+    }
+
+    public Assets setNamespace(String namespace) {
+        this.namespace = namespace;
+        return this;
+    }
+
+    public Assets(String namespace) {
+        this.namespace = namespace;
+    }
 }
