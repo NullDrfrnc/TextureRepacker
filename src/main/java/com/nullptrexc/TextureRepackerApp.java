38e407f916bbe5cc8ca228cd40ee8a2f1c855e77
null/ <null.ptr.exc@outlook.com>
2025-03-25 12:02:07 +0100

    + Added functionality for grabbing and reading the pack.mcmeta when located in the root of the texturepack ! Build doesn't work NoClassDefFoundError when ran Exception in thread "main" java.lang.NoClassDefFoundError: org/json/JSONObject

--- src/main/java/com/nullptrexc/TextureRepackerApp.java
+++ src/main/java/com/nullptrexc/TextureRepackerApp.java
@@ -1,27 +1,21 @@
 package com.nullptrexc;
 
-import com.nullptrexc.model.domain.PackFormat;
 import com.nullptrexc.model.domain.TexturePack;
-import com.nullptrexc.model.domain.mcmeta.Pack;
+import org.json.JSONObject;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class TextureRepackerApp {
     private static TextureRepackerApp instance;
 
-    private TexturePack texturePack;
     private final File currentDir;
+    private TexturePack texturePack;
+
     private TextureRepackerApp() {
         currentDir = new File(Paths.get("").toAbsolutePath().toString());
-    }
-
-    public void run(String[] args) {
-        for (String arg : args) {
-            System.out.println(arg);
-        }
-        System.out.println(currentDir);
-        System.out.println(texturePack.getPack().getPackFormat());
     }
 
     public static void main(String[] args) {
@@ -29,10 +23,31 @@ public class TextureRepackerApp {
         app.run(args);
     }
 
+    public void run(String[] args) {
+        convertMcMetaToJson(findMcmeta());
+    }
+
+    private void convertMcMetaToJson(File mcMeta) {
+        try {
+            String mcMetaContent = new String(Files.readAllBytes(mcMeta.toPath()));
+            assert !mcMetaContent.isEmpty() : "pack.mcmeta is an empty file!";
+            JSONObject json = new JSONObject(mcMetaContent);
+            System.out.println(json);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public static TextureRepackerApp getInstance() {
         if (instance == null) {
             instance = new TextureRepackerApp();
         }
         return instance;
+    }
+
+    public File findMcmeta() {
+        File packMc = new File(currentDir, "pack.mcmeta");
+        assert !packMc.exists() : "Could not find pack.mcmeta!";
+        return packMc;
     }
 }
